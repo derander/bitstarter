2410a7e48cd86dd4594b780ef5d809ea259c0166
Gion <giongross@gmail.com>
2013-08-04 12:22:14 +0000

    updated web.js but.toString

--- web.js
+++ web.js
@@ -1,15 +1,15 @@
 var express = require('express');
 var filesync = require('fs');
-//var buff = require('buffer');
+var buff = require('buffer');
 
-//var hello = new Buffer(filesync.readFileSync('index.html'));
+var hello = new Buffer(filesync.readFileSync('index.html'));
 
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  //response.send(hello.toString());
-    response.send('Hello World 2!');
+  response.send(hello.toString());
+//    response.send('Hello World 2!');
 });
 
 var port = process.env.PORT || 5000;
