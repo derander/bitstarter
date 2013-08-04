8d68e2b064ca0e5a24ecc9a533eb12f41e8e602d
Gion <giongross@gmail.com>
2013-08-04 12:12:11 +0000

    updated web.js and commented Buffer

--- web.js
+++ web.js
@@ -1,17 +1,18 @@
 var express = require('express');
 var filesync = require('fs');
-var buff = require('Buffer');
+//var buff = require('buffer');
 
-var hello = new Buffer(filesync.readFileSync('index.html'));
- 
+//var hello = new Buffer(filesync.readFileSync('index.html'));
+
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send(hello.toString());
+  //response.send(hello.toString());
+    response.send('Hello World!');
 });
 
 var port = process.env.PORT || 5000;
 app.listen(port, function() {
-  console.log("Listening on " + port);
+  console.log("LISTENING ON " + port);
 });
